d18b877f2993c027174165c716a537ef14608fa6
Maggiplant <joekehylke@live.nl>
2022-02-23 14:16:55 +0100

    Commando's kunnen nu aangeroepen worden zonder aanhalingstekens te gebruiken. Het probleem was de standaard readtable-case.

--- reader-macro.lisp
+++ reader-macro.lisp
@@ -1,10 +1,22 @@
-;; Dit werkt al, zolang de syntaxis !"commando" gebruikt wordt. Beter
-;; is als de aanhalingstekens weggelaten kunnen worden. ambrevar deed
-;; dit geloof ik door een eigen (read-until)-functie te gebruiken in
-;; de command-reader macro.
+;; Dit is een reader macro om sh-commando's uit te voeren vanuit een
+;; Common Lisp REPL. In de functie command-reader wordt van een stream
+;; die als argument binnenkomt, een form gemaakt:
+;; (values (uiop:run-program (string stream) :output :string))
+;; Deze functie wordt uitgevoerd nadat de reader een uitroepteken (!)
+;; voorbij komt. Alles wat op dezelfde regel na het uitroepteken staat
+;; wordt stream in command-reader en daarmee een sh-commando dat
+;; uitgevoerd wordt met uiop:run-program.
+
+;; TODO: Daadwerkelijk de hele regel na het uitroepteken de stream
+;; laten worden. Nu wordt alleen datgene dat direct na het
+;; uitroepteken staat het stream-argument van command-reader.
 
 (defun command-reader (stream char)
   (declare (ignore char))
-  (list (quote values) (list (quote uiop:run-program) (string (read stream t nil t)) :output :string)))
+  (let ((orig-rtable-case (readtable-case *readtable*)))
+    (setf (readtable-case *readtable*) :preserve)
+    (return-from command-reader (list (quote values) (list (quote uiop:run-program) (string (read
+								 stream t nil t)) :output :string)))
+    (setf (readtable-case *readtable*) orig-rtable-case)))
 
 (set-macro-character #\! (function command-reader))
