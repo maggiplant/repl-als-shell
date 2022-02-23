1c3e4e772fc53a997cd108a657c0440c7bd836f9
Maggiplant <joekehylke@live.nl>
2022-02-23 13:35:52 +0100

    Onnodige return values weggehaald door (values) te gebruiken

--- reader-macro.lisp
+++ reader-macro.lisp
@@ -3,12 +3,8 @@
 ;; dit geloof ik door een eigen (read-until)-functie te gebruiken in
 ;; de command-reader macro.
 
-;; Nog een verbeterpunt is de onnodige output-info zoals de NIL en
-;; objectinformatie (of zo) die altijd hetzelfde is:
-;; 0 (0 bits, #x0, #o0, #b0)
-
 (defun command-reader (stream char)
   (declare (ignore char))
-  (list (quote uiop:run-program) (string (read stream t nil t)) :output :string))
+  (list (quote values) (list (quote uiop:run-program) (string (read stream t nil t)) :output :string)))
 
 (set-macro-character #\! (function command-reader))
